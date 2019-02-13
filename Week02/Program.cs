228c730247b10d900922f437b9e621c6d6a23ecf
User <User@DEV-022779.OnBase.net>
2019-02-12 19:17:22 -0500

    week 2

--- Week02/Program.cs
+++ Week02/Program.cs
@@ -7,7 +7,27 @@ namespace Week02
         static void Main(string[] args)
         {
             // Create variables and do things with them!
-            
+            Console.WriteLine("Hello World");
+
+            //About me 
+            string myName = "Halle";
+            int myAge = 18;
+            string favcolor = "Pink";
+            string myHobby = "Field Hockey";
+            bool isOldEnoughToDrive = true;
+            double myHeight = 5.75;
+            char firstInitial = 'H';
+
+            Console.WriteLine("My name is " + myName);
+            Console.WriteLine("My age is {0}", myAge);
+            Console.WriteLine("My favorite color is " + favcolor);
+            Console.WriteLine("My hobby is " + myHobby);
+            Console.WriteLine("My height is " + myHeight);
+            Console.WriteLine("My first initial is " + firstInitial);
+            Console.WriteLine("I am old enough to drive " + isOldEnoughToDrive);
+
+
+
         }
     }
 }
