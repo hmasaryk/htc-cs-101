77cb32e24325982bfd346f67699729d6673e6881
User <User@DEV-022781.OnBase.net>
2019-02-19 19:27:40 -0500

    Worked on Week 3 (if else statements)

---
+++ Week 03/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Week_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+
+            // //maske a decision
+
+            // Console.WriteLine("What is the tempatur outside");
+            // int OutsideTempature = Convert.ToInt32(Console.ReadLine());
+
+            // //To make input statements you need both lines (14 and 15)
+
+            // if (OutsideTempature <= 25){
+            //     Console.WriteLine("Put on a Coat!");
+            // }
+            // else if (OutsideTempature >= 26 && OutsideTempature <=50){
+            //     Console.WriteLine("Wear Sunglasses");
+            // }
+            // else{
+            //     Console.WriteLine("Try not to die.");
+            // }
+            // {
+                
+            // }
+            Console.WriteLine("Guess a number between 1 and 10");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            if (Number > 4){
+                Console.WriteLine("Too High!");
+
+            }
+            else if (Number < 4){
+                Console.WriteLine("Too Low!");
+                
+            }
+            else{
+                Console.WriteLine("Just Right!");
+            }
+        }
+    }
+}
