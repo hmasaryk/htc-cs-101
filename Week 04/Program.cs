3d0dfcfb0aa2b7155d2b5c7e2bd2705c5324990a
User <User@DEV-022782.OnBase.net>
2019-02-26 19:23:45 -0500

    This is what we worked on for week 4. More if and else statements

---
+++ Week 04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Week_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        Console.WriteLine("What day of the week is it? (mon,tue,wed,thu,fri,sat,sun) ");
+        string day = Console.ReadLine();
+
+        Console.WriteLine("What time is it (use military time)? ");
+        int time = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("How many hours are you going to be parked here? ");
+        int length = Convert.ToInt32(Console.ReadLine());
+
+        if ((time >= 4 && time<= 7) || (time >= 0 && time<=7 && day == "sat")||(time >= 0 && time<=7 && day == "sun")) {
+            Console.WriteLine("DO NOT PARK HERE! YOUR CAR WILL BE TOWED!!!");
+        }
+        
+        else if ((day == "mon" || day == "tue" || day == "wed" || day == thu) && ((time= 22 && length <=4) || (time= 23 && length <=3) )
+    
+        }
+    }
+}
